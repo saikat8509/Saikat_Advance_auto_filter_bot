53361a7404843c97616dded262147e60b70897c9
saikat8509 <mailmetosaikat007@gmail.com>
2025-05-27 23:18:40 +0530

    Update referral_template.py

--- templates/referral_template.py
+++ templates/referral_template.py
@@ -1,1 +1,13 @@
-referral_template.py
+# templates/referral_template.py
+
+from config import REFERRAL_REWARD_DAYS
+
+def get_referral_template(referral_link: str, referral_count: int):
+    return (
+        "<b>🎁 Earn Premium for Free!</b>\n\n"
+        f"<b>🔗 Your Referral Link:</b>\n<code>{referral_link}</code>\n\n"
+        f"<b>📨 Send this to your friends. When they buy premium using your link, "
+        f"you earn <u>{REFERRAL_REWARD_DAYS} days</u> of premium for each!</b>\n\n"
+        f"<b>👥 Total Referrals:</b> <code>{referral_count}</code>\n\n"
+        "💡 Tip: The more you refer, the more premium you earn!"
+    )
